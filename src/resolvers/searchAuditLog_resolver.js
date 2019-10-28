29bb608fc3c49f6dcd50ddb928caf1f6aab93a54
trevorrwarduk <trevorrwarduk@gmail.com>
2019-10-28 10:58:32 +0000

    Working on Unit Tetsts

--- src/resolvers/searchAuditLog_resolver.js
+++ src/resolvers/searchAuditLog_resolver.js
@@ -1,10 +1,12 @@
 const dbConnect = require("../db/db");
 
+const queryString = `SELECT "searchAudit_ID", "search_user_ID", "search_Detail", "search_end_point", created_on::text, last_updated::text from "searchAudit_log"`;
+
 module.exports = {
     Query: {
-        searchAuditLog: async () => {
+        getSearchAuditLog: async () => {
             try {
-                const result = await dbConnect.query('SELECT * from "searchAudit_log"');
+                const result = await dbConnect.query(queryString);
                 return result.rows;
             } catch (err) {
                 throw new Error(`Database searchAudit_log ERROR -  ${err}`);
--- src/resolvers/searchAuditLog_resolver.test.js
+++ src/resolvers/searchAuditLog_resolver.test.js
@@ -1,26 +1,48 @@
 const query = require("./searchAuditLog_resolver");
 
-describe("index.js", () => {
-    beforeEach(() => {
-        // /* eslint-disable */
-        // // eslint is reporting - 'process' is not defined  no-undef
-        // process.env = Object.assign(process.env, {
-        //     /* eslint-enable */
-        //     NODE_ENV: "local",
-        //     DATABASE: "thedatabase",
-        //     DATABASE_USER: "theuser",
-        //     DATABASE_PASSWORD: "thepassword",
-        //     DATABASE_HOST: "localhost",
-        //     DATABASE_PORT: 5432
-        // });
+const getSearchAuditLogMock = {
+    getSearchAuditLog: [
+        {
+            searchAudit_ID: "1",
+            search_user_ID: "ANON ",
+            search_Detail: "https://theapi/thesearch?param1=1 ",
+            search_end_point: "theapi ",
+            created_on: "2019-10-17 14:38:57.459205+01",
+            last_updated: "2019-10-17 14:38:57.459205+01"
+        },
+        {
+            searchAudit_ID: "2",
+            search_user_ID: "ANON ",
+            search_Detail: "https://theapi/thesearch?param1=1 ",
+            search_end_point: "theapi ",
+            created_on: "2019-10-17 14:39:33.76316+01",
+            last_updated: "2019-10-17 14:39:33.76316+01"
+        }
+    ]
+};
+
+const obj = { getSearchAuditLog: getSearchAuditLogMock };
+const args = {};
+
+describe("searchAuditLog_resolver.js", () => {
+    beforeAll(() => {
+        /* eslint-disable */
+        // eslint is reporting - 'process' is not defined  no-undef
+        process.env = Object.assign(process.env, {
+            /* eslint-enable */
+
+            NODE_ENV: "local",
+            DATABASE: "thedatabase",
+            DATABASE_USER: "theuser",
+            DATABASE_PASSWORD: "thepassword",
+            DATABASE_HOST: "localhost",
+            DATABASE_PORT: 5432
+        });
     });
-    it("Should test the Database connection configuration", () => {
-        console.log(query.Query.searchAuditLog);
-        // const dbConnect = require("./db");
-        // expect(dbConnect.options.user).toEqual("theuser");
-        // expect(dbConnect.options.password).toEqual("thepassword");
-        // expect(dbConnect.options.host).toEqual("localhost");
-        // expect(dbConnect.options.port).toEqual("5432");
-        // expect(dbConnect.options.database).toEqual("thedatabase");
+    // beforeEach(() => {});
+    it("Should test the Database connection configuration", async () => {
+        console.log(query.Query.getSearchAuditLog);
+        // const output = await query.Query.getSearchAuditLog(obj, args);
+        // expect(output).toEqual(obj.getSearchAuditLog);
     });
 });
