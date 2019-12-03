f8ad839670bb8ba8ad57f5c55e8b93c4cb764da0
trevorrwarduk <trevorrwarduk@gmail.com>
2019-12-03 14:59:19 +0000

    smart a*&^ Tim's await option

--- src/resolvers/HDR/datamodelFilters_resolver.js
+++ src/resolvers/HDR/datamodelFilters_resolver.js
@@ -2,39 +2,31 @@ const fetch = require("node-fetch");
 
 module.exports = {
     Query: {
-        hdrFilterValues: () => {
-            return fetch(
-                `${process.env.API_BASE_URL}${process.env.API_FILTER_VALUES}${process.env.API_FILTER_FILTERS}`,
-                {
-                    method: "GET",
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json"
+        hdrFilterValues: async () => {
+            try {
+                const apiData = await fetch(
+                    `${process.env.API_BASE_URL}${process.env.API_FILTER_VALUES}${process.env.API_FILTER_FILTERS}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Accept: "application/json",
+                            "Content-Type": "application/json"
+                        }
                     }
-                }
-            )
-                .then(res => {
-                    const status = res.status;
-                    const message = res.statusText;
-
-                    return res
-                        .json()
-                        .then(data => {
-                            return Object.keys(data).map(function(inData) {
-                                return { fieldName: inData, fieldValues: data[inData] };
-                            });
-                        })
-                        .then(dataO => {
-                            return {
-                                status: status,
-                                message: message,
-                                data: dataO
-                            };
-                        });
-                })
-                .catch(err => {
-                    return "ERROR", err;
-                });
+                );
+                const apiDataJSON = await apiData.json();
+                const dataO = Object.keys(apiDataJSON).map(inData => ({
+                    fieldName: inData,
+                    fieldValues: apiDataJSON[inData]
+                }));
+                return {
+                    status: apiData.status,
+                    message: apiData.statusText,
+                    data: dataO
+                };
+            } catch (e) {
+                return "ERROR", e;
+            }
         }
     }
 };
