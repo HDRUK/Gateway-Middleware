9efed1fcdd51f451facc5f452d523444abfeafbb
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2019-12-04 14:25:46 +0000

    Update to use passport

--- src/index.js
+++ src/index.js
@@ -7,15 +7,31 @@ const typeDefs = require("./schema/schema");
 const resolvers = require("./resolvers/resolvers");
 const cors = require("cors");
 const passport = require("passport");
+// const session = require("express-session");
 
 const port = process.env.PORT;
 
 const app = express();
 
+// app.use(
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         resave: false,
+//         saveUninitialized: true
+//     })
+// );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.deserializeUser((user, next) => {
+    console.log("called");
+    next(null, user);
+});
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    cors: { origin: process.env.FRONTEND_APP, credentials: true },
     formatError: err => {
         if (err.originalError && err.originalError.error_message) {
             err.message = err.originalError.error_message;
