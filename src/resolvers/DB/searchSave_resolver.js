54f13d9de89c46384e34222b4c0fe38906401e47
Tim Howard <tim.howard@ibm.com>
2019-12-04 11:26:04 +0000

    Add a check for duplicate save name

--- src/resolvers/DB/searchSave_resolver.js
+++ src/resolvers/DB/searchSave_resolver.js
@@ -4,10 +4,25 @@ const searchSavedQueryString = (searchAuditId, userId, name) => `
     INSERT INTO searchsaved (searchsaved_searchaudit_id, searchsaved_user_id${name ? ", searchsaved_name" : ""})
     VALUES ('${searchAuditId}', '${userId}'${name ? `, '${name}'` : ""})`;
 
+const searchNameCheckQueryString = (userId, name) => `
+    SELECT searchsaved_searchaudit_id
+    FROM searchsaved
+    WHERE searchsaved_user_id='${userId}' AND searchsaved_name='${name}'`;
+
 module.exports = {
     Mutation: {
         searchSave: async (_, { searchAuditId, userId, name }) => {
             try {
+                if (name) {
+                    const searchNameCheckSQL = searchNameCheckQueryString(userId, name);
+                    const searchNameCheckData = await dbConnect.query(searchNameCheckSQL);
+                    if (searchNameCheckData && searchNameCheckData.rows.length > 0)
+                        return {
+                            status: 403,
+                            message: "Unable to save: name already exists"
+                        };
+                }
+
                 const searchSavedSQL = searchSavedQueryString(searchAuditId, userId, name);
                 await dbConnect.query(searchSavedSQL);
 
