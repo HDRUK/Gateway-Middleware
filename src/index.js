f6ee3b48d4f969969df90972a1245fc58995b77a
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2019-12-04 12:20:31 +0000

    Update for cookie

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const { ApolloServer } = require("apollo-server");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 require("dotenv").config();
 
 const logger = require("./utils/logger");
 const typeDefs = require("./schema/schema");
 const resolvers = require("./resolvers/resolvers");
+const cors = require("cors");
+const passport = require("passport");
 
 const port = process.env.PORT;
+
+const app = express();
 
 const server = new ApolloServer({
     typeDefs,
@@ -21,9 +26,22 @@ const server = new ApolloServer({
     }
 });
 
-server.listen(port).then(({ url }) => {
+const corsOptions = {
+    origin: process.env.FRONTEND_APP,
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+
+server.applyMiddleware({
+    app,
+    path: "/",
+    cors: false
+});
+
+app.listen(port, () =>
     logger.log({
         level: "info",
-        message: `Server running on Port ${url}`
-    });
-});
+        message: `Server running on Port ${port}`
+    })
+);
