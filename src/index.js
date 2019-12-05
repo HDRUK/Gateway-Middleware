3da4cd1c47f035912bce2976f9d77cbb589c84e5
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2019-12-05 11:41:54 +0000

    Fetch user info

--- src/index.js
+++ src/index.js
@@ -6,32 +6,45 @@ const logger = require("./utils/logger");
 const typeDefs = require("./schema/schema");
 const resolvers = require("./resolvers/resolvers");
 const cors = require("cors");
-const passport = require("passport");
-// const session = require("express-session");
 
 const port = process.env.PORT;
 
 const app = express();
+const corsOptions = {
+    origin: process.env.FRONTEND_APP,
+    credentials: true
+};
 
-// app.use(
-//     session({
-//         secret: process.env.SESSION_SECRET,
-//         resave: false,
-//         saveUninitialized: true
-//     })
-// );
+app.use(cors(corsOptions));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.deserializeUser((user, next) => {
-    console.log("called");
-    next(null, user);
-});
+const getUser = async token => {
+    try {
+        if (token) {
+            const response = await fetch(process.env.USER_INFO, {
+                headers: { authorization: `Bearer ${token}` }
+            });
+            const json = await response.json();
+            console.log("Success:", JSON.stringify(json));
+        }
+        console.log("Here");
+        return null;
+    } catch (err) {
+        return null;
+    }
+};
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: ({ req }) => {
+        const tokenWithBearer = req.headers.authorization || "";
+        const token = tokenWithBearer.split(" ")[1];
+        const user = getUser(token);
+
+        return {
+            user
+        };
+    },
     formatError: err => {
         if (err.originalError && err.originalError.error_message) {
             err.message = err.originalError.error_message;
@@ -41,13 +54,6 @@ const server = new ApolloServer({
         return err;
     }
 });
-
-const corsOptions = {
-    origin: process.env.FRONTEND_APP,
-    credentials: true
-};
-
-app.use(cors(corsOptions));
 
 server.applyMiddleware({
     app,
